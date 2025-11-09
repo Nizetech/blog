4f474f67040a3555ee2bbcf25e30122c480c99a8
Fortune Nwanchokor <fortunepraise97@gmail.com>
2025-11-09 20:39:23 +0100

    added blog provider

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./Src/index_screen";
+import { StatusBar } from "expo-status-bar";
+import { BlogProvider } from "./Src/context/blog_context";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator
+        initialRouteName="IndexScreen"
+        screenOptions={{
+          title: "Blog",
+        }}
+      >
+        <Stack.Screen name="IndexScreen" component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
